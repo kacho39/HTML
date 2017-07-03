a5f911bab07042b2008e1c269dec925652cc28c5
kacho39 <kacho39-c@my.cityu.edu.hk>
2017-07-03 15:30:22 +0800

    AWTDemo
    GridBagLayout, Label, TextField, TextArea, Button,

--- Java/AWTDemo.java
+++ Java/AWTDemo.java
@@ -2,7 +2,7 @@
 // http://www.kaiching.org/2012/06/java-api-frame.html
 
 import java.awt.*; // Frame, Button
-import java.awt.event.*; // WindowAdapter, windowClosing(WindowAdapter入面, WindowEvent
+import java.awt.event.*; // WindowAdapter, windowClosing(inside WindowAdapter), WindowEvent
  
 public class AWTDemo {
 
@@ -11,22 +11,63 @@ public class AWTDemo {
         frame.addWindowListener(new AdapterDemo()); // close program by the cross button
         
         // layout type
-        frame.setLayout(new GridLayout(4, 1)); // GridLayout(int rows, int cols)
+        frame.setLayout(new GridBagLayout()); // very specific very useful
+        //frame.setLayout(null);
+        //frame.setLayout(new GridLayout(4, 1)); // GridLayout(int rows, int cols)
+        // http://www.kaiching.org/2012/06/java-api-awt-layout.html
 
         frame.setSize(400, 180);
-        
+
+
+
+
         // make labels
         Label label1;
         label1 = new Label("There is no spoon.", Label.LEFT);
         /*          3 alignments parameter
             Label.LEFT
             Label.RIGHT
-            Babel.CENTER
+            Label.CENTER
+        */
+        // GridBagConstraints for each component
+        GridBagConstraints c1 = new GridBagConstraints();
+        c1.gridx = 0;   //  from left to right
+        c1.gridy = 0;   //  from top to bottom
+        c1.gridwidth = 1;   //  how many grid to be filled
+        c1.gridheight = 1;
+        c1.fill = GridBagConstraints.BOTH;  //  how the grid is filled
+        c1.anchor = GridBagConstraints.CENTER;    //  location in grid
+        /*          fill attribute
+            CENTER, EAST, NORTH, NORTHEAST, NORTHWEST, SOUTH, SOUTHEAST, SOUTHWEST, WEST
+        */
+        /*          anchor attribute
+            BOTH, HORIZONTAL, NONE, VERTICAL
         */
 
+
+
+
         // make text input place
+        // TextField  一行
+        TextField text1 = new TextField("Please enter", 10);
+        /*          4 constructor
+            TextField() 
+            TextField(int columns) 
+            TextField(String text) 
+            TextField(String text, int columns) 
+        */
+
+        // GridBagConstraints
+        GridBagConstraints c2 = new GridBagConstraints();
+        c2.gridx = 1;
+        c2.gridy = 0;
+        c2.gridwidth = 1;
+        c2.gridheight = 1;
+        c2.fill = GridBagConstraints.BOTH;
+        c2.anchor = GridBagConstraints.CENTER;
+
         // TextArea  多行
-        TextArea text1 = new TextArea("Please enter", 10, 20, TextArea.SCROLLBARS_BOTH);
+        TextArea text2 = new TextArea("Please enter", 10, 10, TextArea.SCROLLBARS_BOTH);
         /*          5 constructor
             TextArea()
             TextArea(int rows, int columns)
@@ -40,17 +81,35 @@ public class AWTDemo {
             TextArea.SCROLLBARS_NONE
             TextArea.SCROLLBARS_VERTICAL_ONLY
         */
+
+        // GridBagConstraints
+        GridBagConstraints c3 = new GridBagConstraints();
+        c3.gridx = 0;
+        c3.gridy = 1;
+        c3.gridwidth = 2;
+        c3.gridheight = 1;
+        c3.fill = GridBagConstraints.BOTH;
+        c3.anchor = GridBagConstraints.CENTER;
         
+
+
         // make buttons
         Button button1 = new Button("name display");
+
+        // GridBagConstraints
+        GridBagConstraints c4 = new GridBagConstraints();
+        c4.gridx = 0;
+        c4.gridy = 2;
+        c4.gridwidth = 1;
+        c4.gridheight = 1;
+        c4.fill = GridBagConstraints.BOTH;
+        c4.anchor = GridBagConstraints.CENTER;
          
         // add to the frame
-        frame.add(label1);
-        frame.add(label2);
-        frame.add(label3);
-        frame.add(text1);
-        frame.add(button1); 
-        frame.add(button2);
+        frame.add(label1, c1);
+        frame.add(text1, c2);
+        frame.add(text2, c3);
+        frame.add(button1, c4); 
          
         frame.pack(); // make the window size to fit the preferred size
         frame.setVisible(true); // show of hide the window
